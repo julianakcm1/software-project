2a7b35840a64e1fef9bdab32528467ff3a6ebf23
Juliana Melo <54086293+julianakcm1@users.noreply.github.com>
2022-12-13 22:10:52 -0300

    Update Aluno.java

--- src/usuario/Aluno.java
+++ src/usuario/Aluno.java
@@ -1,5 +1,7 @@
 package usuario;
 
+import java.util.Scanner;
+
 public class Aluno extends Usuario{
     private String matricula;
     private String grau;
@@ -26,10 +28,52 @@ public class Aluno extends Usuario{
         this.grau = grau;
     }
 
+    public void atualizar(){
+        Scanner leitor = new Scanner(System.in);
+        int entrada;
+        while (true){
+            System.out.println("\nDigite 0 para alterar o login");
+            System.out.println("Digite 1 para alterar o nome");
+            System.out.println("Digite 2 para alterar a matricula");
+            System.out.println("Digite 3 para alterar o grau");
+            System.out.println("Digite 4 para alterar a senha");
+            System.out.println("Digite 5 para finalizar\n");
+            entrada = leitor.nextInt();
+            leitor.nextLine();
+            if (entrada == 0){
+                System.out.println("\nDigite o novo login\n");
+                String novoLogin = leitor.nextLine();
+                this.setLogin(novoLogin);
+            } else if (entrada == 1) {
+                System.out.println("\nDigite o novo nome\n");
+                String novoNome = leitor.nextLine();
+                this.setNome(novoNome);
+
+            } else if (entrada == 2) {
+                System.out.println("\nDigite a nova matricula\n");
+                String novaMatricula = leitor.nextLine();
+                this.setMatricula(novaMatricula);
+            }else if (entrada == 3) {
+                System.out.println("\nDigite o novo grau\n");
+                String novoGrau = leitor.nextLine();
+                this.setGrau(novoGrau);
+            }else if(entrada == 4){
+                System.out.println("\nDigite a nova senha\n");
+                String novaSenha = leitor.nextLine();
+                System.out.println("\nDigite a antiga senha\n");
+                String antigaSenha = leitor.nextLine();
+                String resultado = this.alterarSenha(antigaSenha, novaSenha);
+                System.out.println(resultado);
+            }else {
+                System.out.println("\nErro entrada invalida\n");
+            }
+        }
+    }
+
     @Override
     public void exibir() {
         super.exibir();
-        System.out.println("Matricula: " + this.matricula);
-        System.out.println("Grau: " + this.grau);
+        System.out.println("\nMatricula: " + this.matricula);
+        System.out.println("\nGrau: " + this.grau);
     }
 }
