fa9b18997ee8b6df5fd2d13b3b8b536bd0912808
Juliana Melo <54086293+julianakcm1@users.noreply.github.com>
2022-12-13 22:11:14 -0300

    Add files via upload

---
+++ src/usuario/Deslogado.java
@@ -0,0 +1,16 @@
+package usuario;
+
+public class Deslogado extends Usuario{
+    public Deslogado() {
+        super(null, null, null, null, null);
+    }
+
+    @Override
+    public void atualizar() {
+        throw new IllegalArgumentException("\nNao eh possivel atualizar um usuario nulo\n");
+    }
+
+    public boolean isnull(){
+        return true;
+    }
+}
